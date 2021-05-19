2f61814fb0157d0ef64da26b4f6acc8aa18c44d2
Pablo de la Peña <p@shedcollective.org>
2021-05-19 13:02:40 +0100

    Add database migration for tags

---
+++ src/Database/Migration/Migration0.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Migration:  0
+ * Started:    19/05/2021
+ *
+ * @package    Nails
+ * @subpackage module-tag
+ * @category   Database Migration
+ * @author     Nails Dev Team
+ */
+
+namespace Nails\Tag\Database\Migration;
+
+use Nails\Common\Console\Migrate\Base;
+
+class Migration0 extends Base
+{
+    /**
+     * Execute the migration
+     *
+     * @return Void
+     */
+    public function execute()
+    {
+        $this->query("
+            CREATE TABLE `{{NAILS_DB_PREFIX}}tag` (
+                `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                `slug` varchar(150) DEFAULT NULL,
+                `label` varchar(150) DEFAULT NULL,
+                `created` datetime NOT NULL,
+                `created_by` int(11) unsigned DEFAULT NULL,
+                `modified` datetime NOT NULL,
+                `modified_by` int(11) unsigned DEFAULT NULL,
+                PRIMARY KEY (`id`),
+                KEY `created_by` (`created_by`),
+                KEY `modified_by` (`modified_by`),
+                CONSTRAINT `{{NAILS_DB_PREFIX}}tag_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `nails_user` (`id`) ON DELETE SET NULL,
+                CONSTRAINT `{{NAILS_DB_PREFIX}}tag_ibfk_2` FOREIGN KEY (`modified_by`) REFERENCES `nails_user` (`id`) ON DELETE SET NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        ");
+    }
+}
