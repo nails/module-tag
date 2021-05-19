b5626f8c2f07e114d7ddd68d6edf3af3ce641c16
Pablo de la Peña <p@shedcollective.org>
2021-05-19 13:02:52 +0100

    Add module constants

---
+++ src/Constants.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nails\Tag;
+
+/**
+ * Class Constants
+ *
+ * @package Nails\Tag
+ */
+class Constants
+{
+    /**
+     * The slug for this module
+     *
+     * @var string
+     */
+    const MODULE_SLUG = 'nails/module-tag';
+}
